0b2c6579bababa772a81e754ae0aed24a8f9edb3
SebaMutuku <sebastian.muthiani@gmail.com>
2023-06-09 18:26:58 +0300

    Handle exceptions

---
+++ src/main/java/com/logiceacards/api/ControllerAdvice.java
@@ -0,0 +1,57 @@
+package com.logiceacards.api;
+
+
+import com.logiceacards.dto.ResponseDTO;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler({AccessDeniedException.class, UsernameNotFoundException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ResponseDTO> handledException(
+            Exception ex) {
+        var response = new ResponseDTO(null, ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({NoSuchElementException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ResponseDTO> handledException(NoSuchElementException exception) {
+        var response = new ResponseDTO(null, exception.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ResponseDTO> handledException(RuntimeException exception) {
+        var response = new ResponseDTO(null, exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, HttpMediaTypeNotAcceptableException.class, IllegalArgumentException.class, DataIntegrityViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ResponseDTO> handleHttpExceptions(Exception exception) {
+        var response = new ResponseDTO(null, exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ResponseDTO> handledException(NoHandlerFoundException exception) {
+        var response = new ResponseDTO(null, exception.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
+}
--- src/main/java/com/logiceacards/api/RestControllerAdvice.java
+++
@@ -1,6 +0,0 @@
-package com.logiceacards.api;
-
-
-@org.springframework.web.bind.annotation.RestControllerAdvice
-public class RestControllerAdvice {
-}
