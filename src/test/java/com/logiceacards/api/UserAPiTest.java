4748c73749ea64abe90fc397fdadc38773f1e280
SebaMutuku <sebastian.muthiani@gmail.com>
2023-06-11 23:53:58 +0300

    Fix failing tests in userAPi tets

--- src/test/java/com/logiceacards/api/UserAPiTest.java
+++ src/test/java/com/logiceacards/api/UserAPiTest.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.intercept.RunAsImplAuthenticationProvider;
@@ -24,42 +25,29 @@ import static org.mockito.Mockito.*;
 @SpringJUnitConfig
 @SpringBootTest
 class UserAPiTest {
+    @MockBean
+    private UserRepo userRepo;
     @Test
-    void testAuthenticate() {
+    void testAuthenticateSuccessfully() {
         TokenService tokenService = new TokenService();
-        UserRepo userRepo = mock(UserRepo.class);
         UserApi userAPi = new UserApi(
                 new UserService(mock(ProviderManager.class), tokenService, userRepo, new BCryptPasswordEncoder()));
-        userAPi.authenticate(new UserDTO("janedoe", "iloveyou"));
+        userAPi.authenticate(new UserDTO("logicea", "pass123"));
     }
 
     @Test
-    void testAuthenticate2() {
+    void testAuthenticateReturnsBadCredentials() {
         ArrayList<AuthenticationProvider> providers = new ArrayList<>();
         providers.add(new RunAsImplAuthenticationProvider());
         ProviderManager authenticationManager = new ProviderManager(providers);
-        UserRepo userRepo = mock(UserRepo.class);
-        when(userRepo.findByUsername(Mockito.any())).thenReturn(Optional.of(new User()));
-        TokenService tokenService = new TokenService();
-        UserApi userAPi = new UserApi(
-                new UserService(authenticationManager, tokenService, userRepo, new BCryptPasswordEncoder()));
-        userAPi.authenticate(new UserDTO("logiceas", "pass123"));
-    }
-
-    @Test
-    void testAuthenticate3() {
-        ArrayList<AuthenticationProvider> providers = new ArrayList<>();
-        providers.add(new RunAsImplAuthenticationProvider());
-        ProviderManager authenticationManager = new ProviderManager(providers);
-        UserRepo userRepo = mock(UserRepo.class);
         when(userRepo.findByUsername(Mockito.any())).thenReturn(Optional.empty());
         TokenService tokenService = new TokenService();
         UserApi userAPi = new UserApi(
                 new UserService(authenticationManager, tokenService, userRepo, new BCryptPasswordEncoder()));
-        ResponseEntity<ResponseDTO> actualAuthenticateResult = userAPi.authenticate(new UserDTO("janedoe", "iloveyou"));
+        ResponseEntity<ResponseDTO> actualAuthenticateResult = userAPi.authenticate(new UserDTO("logicea", "pass123"));
         assertTrue(actualAuthenticateResult.hasBody());
         assertTrue(actualAuthenticateResult.getHeaders().isEmpty());
-        assertEquals(401, actualAuthenticateResult.getStatusCodeValue());
+        assertEquals(401, actualAuthenticateResult.getStatusCode().value());
         ResponseDTO body = actualAuthenticateResult.getBody();
         assertNull(body.payload());
         assertEquals("Unauthorized. Please check your credentials", body.message());
@@ -68,15 +56,15 @@ class UserAPiTest {
     }
 
     @Test
-    void testAuthenticate4() {
+    void testAuthenticateReturnsCorrectMessage() {
         UserService userService = mock(UserService.class);
         when(userService.authenticate(Mockito.any()))
                 .thenReturn(new ResponseDTO("Payload", "Not all who wander are lost", HttpStatus.CONTINUE));
         UserApi userAPi = new UserApi(userService);
-        ResponseEntity<ResponseDTO> actualAuthenticateResult = userAPi.authenticate(new UserDTO("janedoe", "iloveyou"));
+        ResponseEntity<ResponseDTO> actualAuthenticateResult = userAPi.authenticate(new UserDTO("logicea", "pass123"));
         assertTrue(actualAuthenticateResult.hasBody());
         assertTrue(actualAuthenticateResult.getHeaders().isEmpty());
-        assertEquals(100, actualAuthenticateResult.getStatusCodeValue());
+        assertEquals(100, actualAuthenticateResult.getStatusCode().value());
         verify(userService).authenticate(Mockito.any());
     }
 
